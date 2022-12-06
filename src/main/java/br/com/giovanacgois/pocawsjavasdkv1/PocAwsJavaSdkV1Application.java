b82c7164a2a2f8fb85a59db089484b30bd94de26
giovanacgois <giovanacgois@gmail.com>
2022-12-05 23:29:26 -0300

    First commit

---
+++ src/main/java/br/com/giovanacgois/pocawsjavasdkv1/PocAwsJavaSdkV1Application.java
@@ -0,0 +1,13 @@
+package br.com.giovanacgois.pocawsjavasdkv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocAwsJavaSdkV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocAwsJavaSdkV1Application.class, args);
+	}
+
+}
